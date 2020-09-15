d21bc3aa023b927a5f434d8eab7c86bbc7ab270a
Sanket <Sanket@LAPTOP-8V5DPHDS>
2020-09-15 13:43:18 -0400

    API Commit

--- MavenFramework/src/test/java/API/API_Basics.java
+++ MavenFramework/src/test/java/API/API_Basics.java
@@ -4,6 +4,8 @@ import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import Files.Payload;
+
 
 public class API_Basics {
 
@@ -24,22 +26,7 @@ public class API_Basics {
 		RestAssured.baseURI="https://rahulshettyacademy.com";
 
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-		.body("{\r\n" + 
-				"    \"location\": {\r\n" + 
-				"        \"lat\": -38.383494,\r\n" + 
-				"        \"lng\": 33.427362\r\n" + 
-				"    },\r\n" + 
-				"    \"accuracy\": 50,\r\n" + 
-				"    \"name\": \"Sanket12\",\r\n" + 
-				"    \"phone_number\": \"(+91) 983 893 3937\",\r\n" + 
-				"    \"address\": \"29, side layout, cohen 09\",\r\n" + 
-				"    \"types\": [\r\n" + 
-				"        \"shoe park\",\r\n" + 
-				"        \"shop\"\r\n" + 
-				"    ],\r\n" + 
-				"    \"website\": \"http://google.com\",\r\n" + 
-				"    \"language\": \"French-IN\"\r\n" + 
-				"}").when().post("/maps/api/place/add/json")
+		.body(Payload.Add_Place()).when().post("/maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
 		.header("Server", "Apache/2.4.18 (Ubuntu)");
 
